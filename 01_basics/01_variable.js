7962a8b248667b59512bdf8d89ffdc302759d78d
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-02 20:32:26 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+let accountName = "sameer"
+var accountState = "bihar"
+accountCity = "patna"
+
+let accountPin;
+
+console.table([accountId, accountName, accountState, accountCity, accountPin]);
