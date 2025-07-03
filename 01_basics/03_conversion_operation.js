63b61b7ab82b2fea9fc92b1002780671d3b89fc1
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-03 15:20:05 +0000

    conversion done

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,17 +1,50 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let variableName = Number(score)
 
-console.log(typeof variableName)
+//console.log(typeof variableName)
 
-console.log(typeof score)
+//console.log(typeof score)
 
-console.log(variableName)
+//console.log(variableName)
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//*******************************Operations***************************/
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = " sam"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log(1 + 2 + "2" + 1 + 1 + "1" + 1 + "1");
+
+
+
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+
+
+
+
+
