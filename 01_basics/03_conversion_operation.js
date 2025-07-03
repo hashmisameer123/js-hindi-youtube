161f4390e8127edae4110c3ae7f3d11993c006a4
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-03 12:42:32 +0000

    conversion operatio done

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let variableName = Number(score)
+
+console.log(typeof variableName)
+
+console.log(typeof score)
+
+console.log(variableName)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
