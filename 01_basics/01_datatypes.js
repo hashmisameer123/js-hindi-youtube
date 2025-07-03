f37e89d1d201dc609f265a4d8ee668a0c4913eae
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-03 09:02:51 +0000

    datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefiend);
+console.log(typeof null);
