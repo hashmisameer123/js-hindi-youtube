93bfca2e3271230284887030d6da990645988f5e
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-04 12:52:01 +0000

    array practice done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArr = new Array(0,1,2,3,4)
+// console.log(myArr[1]);
+
+
+//Array methods
+
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+// 
+// myArr.unshift(8)
+// myArr.shift()
+// console.log(myArr);
+
+//slice,splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)   //slice
+console.log(myn1);
+
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
