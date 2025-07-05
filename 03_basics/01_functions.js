34b6212bc1d4cd837134c11384a161f7ec960277
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-05 05:05:53 +0000

    change done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,16 +47,38 @@ const user={
     price: 200
 }
 
-function handleObject(anyObject){
-    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
+// function handleObject(anyObject){
+//     console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
     
-}
+// }
 
-handleObject(user)
+// handleObject(user)
 
-const myNewArray = [100, 200, 300, 400]
+// const myNewArray = [100, 200, 300, 400]
 
 function returnSecondValue(getArray){
     return getArray[4]
 }
-console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue(myNewArray));
+
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("INNER:", a);
+}
+console.log(a);
+
+
+if(true){
+    const username = "sam"
+    if(username === "sam"){
+        const website = " utube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
