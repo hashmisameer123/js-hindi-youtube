0ea79c3ae80911d9be203f7c67061be97a88b477
hashmisameer123 <137373916+hashmisameer123@users.noreply.github.com>
2025-07-05 10:40:08 +0000

    changes done

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,43 @@
+const username = []
+
+// if(username){
+//     console.log("got email");
+// }else{
+//     console.log("not got email");
+    
+// }
+
+//falsy value
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value
+
+// "0", 'false, " ", [], {}, funtion(){}
+
+// if(username.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+    
+// }
+
+let val1;
+val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+console.log(val1);
+
+
+//Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+;
